f9b159eb8082b98cac815ab250e403f90d2edcc5
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2026-02-17 16:38:01 +0700

    Update Privacy.jsx

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Privacy() {
   return (
-    <div className="max-w-2xl py-4">
+    <div className="max-w-2xl py-2">
       <h1 className="text-3xl font-black uppercase tracking-tighter mb-8 text-slate-800">Privacy Policy</h1>
       <div className="space-y-6 text-sm text-slate-600 leading-relaxed">
-        <p>Aplikasi ini memproses foto Anda 100% di browser. Kami tidak menyimpan foto di server.</p>
-        <p>LocalStorage digunakan untuk menyimpan riwayat pekerjaan Anda di perangkat Anda sendiri.</p>
+        <p>Proses gambar dilakukan 100% secara lokal di dalam browser Anda.</p>
+        <p>LocalStorage digunakan untuk menyimpan riwayat hasil edit Anda di perangkat sendiri agar data tetap tersedia saat Anda kembali.</p>
       </div>
     </div>
   );
